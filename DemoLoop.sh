24509e4f1892c4a4532725ab3f37f1591a04065b
vismaya01 <kvvismaya98@gmail.com>
2020-10-14 21:07:38 +0530

    test branch first commit

---
+++ DemoLoop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+for eachElement in `ls *.txt`;
+do
+	fileNameWithoutExtension=`echo $eachElement |awk -F. '{ print $1}'`;
+	echo $fileNameWithoutExtension;
+	if [ -d $fileNameWithoutExtension ]
+	then
+		rm -r $fileNameWithoutExtension
+	fi
+	 //creation of directory
+        mkdir $fileNameWithoutExtension;
+
+	//moving file to created folder
+	mv $eachElement $fileNameWithoutExtension;
+done
+
